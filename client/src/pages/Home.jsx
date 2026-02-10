373fd4d84b183ee314dc10311d5cf3db02ba87d6
Rushi <shivanangaaji@gmail.com>
2026-02-10 21:34:36 +0530

    Day3

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
+import FeaturedSection from '../components/FeaturedSection'
+import Banner from '../components/Banner'
 
 function Home() {
   return (
     <>
       <Hero/>
+      <FeaturedSection/>
+      <Banner/>
     </>
   )
 }
