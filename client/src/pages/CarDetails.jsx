f7b8b7aa3e6a0f6b6c507c679d13dc7386796285
Rushi <shivanangaaji@gmail.com>
2026-02-15 22:31:51 +0530

    Worked on car details

--- client/src/pages/CarDetails.jsx
+++ client/src/pages/CarDetails.jsx
@@ -8,7 +8,11 @@ function CarDetails() {
   const { id } = useParams()
   const navigate = useNavigate()
   const [car, setCar] = useState(null)
+  const currency =import.meta.env.VITE_CURRENCY
 
+  const handleSubmit=async(e)=>{
+    e.preventDefault();
+  }
   useEffect(() => {
     const foundCar = dummyCarData.find(c => c._id === id)
     setCar(foundCar)
@@ -88,15 +92,33 @@ function CarDetails() {
         </div>
 
         {/* RIGHT SIDE (Booking Form Placeholder) */}
-        <div>
-          <div className='bg-gray-100 p-6 rounded-lg'>
-            Booking Form Coming Soon
-          </div>
-        </div>
+        <form onSubmit={handleSubmit} className='shadow-lg h-max sticky top-18 rounded-xl p-6 space-y-6 text-gray-500'>
+          <p className='flex items-center justify-between text-2xl text-gray-800 font-semibold'>{currency}{car.pricePerDay} <span className='text-base text-gray-500'>per day</span></p>
+          <hr className='border-borderColor my-6'/>
 
+          {/*Booking Starting date*/}
+          <div className='flex flex-col gap-2'>
+            <label htmlFor="pickup-date">Pickup Date</label>
+            <input type="date" className='border border-borderColor px-3 py-2 rounded-lg ' required id='pickup-date' min={new Date().toISOString().split('T')[0]}/>
+          </div>
+
+          {/* Return Date*/}
+          <div className='flex flex-col gap-2'>
+            <label htmlFor="return-date">Return Date</label>
+            <input type="date" className='border border-borderColor px-3 py-2 rounded-lg ' required id='return-date'/>
+          </div>
+
+          {/* Book Now Button*/}
+          <button className='w-full bg-primary hover:bg-primary-dull transition-all py-3 font-medium text-white rounded-xl cursor-pointer'>
+            Book Now
+          </button>
+          
+          <p className='text-center text-sm'>No credit card required to reserve</p>
+        </form>
+          
       </div>
     </div>
-  )
+  );<Loader/>
 }
 
 export default CarDetails
